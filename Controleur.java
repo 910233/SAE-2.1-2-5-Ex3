7d7a3d0700c34193cf8545c3745683c145aab108
Ted Herambert <tedherambert2004@gmail.com>
2024-06-03 09:39:49 +0200

    création du controleur

---
+++ Controleur.java
@@ -0,0 +1,11 @@
+public class Controleur 
+{	
+	private IHMGUI ihm;
+	private Metier metier;
+	
+	public Controleur ()
+	{
+		this.metier = new Metier ();
+		this.ihm    = new IHMGUI (this);
+	}
+}
---
+++ IHMGUI.java
@@ -0,0 +1,9 @@
+public class IHMGUI
+{
+	Controleur ctrl;
+
+	public IHMGUI(Controleur ctrl)
+	{
+		this. ctrl = ctrl;
+	}
+}
